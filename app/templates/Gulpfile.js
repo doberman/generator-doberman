1536f558df43ae89dccb7be9fdcf203724edd50c
Jacob Gunnarsson <jacob.gunnarsson@hyperisland.se>
2016-03-30 11:34:50 +0200

    Add gulp-connect with livereload on port 3000

--- app/templates/Gulpfile.js
+++ app/templates/Gulpfile.js
@@ -7,17 +7,27 @@ gulp.task('styles', function() {
   return gulp.src('app/stylesheets/**/*.scss')
     .pipe($.sass())
     .pipe($.autoprefixer())
-    .pipe(gulp.dest('build/stylesheets'));
+    .pipe(gulp.dest('build/stylesheets'))
+    .pipe($.connect.reload());
 });
 
 gulp.task('html', function() {
   return gulp.src('app/**/*.html')
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .pipe($.connect.reload());
+});
+
+gulp.task('connect', function() {
+  $.connect.server({
+    root: 'build',
+    port: 3000,
+    livereload: true
+  });
 });
 
 gulp.task('build', ['styles', 'html']);
 
-gulp.task('default', ['build'], function() {
+gulp.task('default', ['build', 'connect'], function() {
   gulp.watch('app/styles/**/*.scss', ['styles']);
   gulp.watch('app/**/*.html', ['html']);
 });
